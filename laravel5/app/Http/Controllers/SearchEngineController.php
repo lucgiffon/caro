756c1d5c00fee9b79173519394684867fe95a7a1
lucgiffon <luc.giffon@gmail.com>
2016-02-17 02:32:49 +0100

    tests de correction du bug de laravel-search

--- laravel5/app/Http/Controllers/SearchEngineController.php
+++ laravel5/app/Http/Controllers/SearchEngineController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Mmanos\Search;
+
 
 class SearchEngineController extends Controller
 {
@@ -14,6 +16,11 @@ class SearchEngineController extends Controller
     }
 
     public function postForm(Request $request) {
+        \Search::insert(1, array(
+            'title' => 'My title',
+            'content' => 'The quick brown fox...',
+            'status' => 'published',
+        ));
         return response()->view('home', ["response" => $request->input('query')]);
     }
 }
